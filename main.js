7df5f4de9863c40c6ec877e84f737474737e4543
Shubham Kushwaha <ansh.kush.2410@gmail.com>
2023-12-16 10:45:36 +0530

    changed according to assingment

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log('github check 123..');
+const header = document.getElementById('header');
+header.style.backgroundColor = 'green';
+header.style.borderBottom = '3px solid orange';
+
+const mainHeading = document.getElementById('main-heading');
+mainHeading.textContent = 'Fruit World';
+mainHeading.style.color = 'orange';
+
+const basketHeading = document.getElementById('basket-heading');
+basketHeading.style.color = 'green';
+
+const thanks = document.getElementById('thanks');
+thanks.innerHTML = '<p>Please visit us again</p>';
